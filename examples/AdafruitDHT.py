83ce7e9299ca0b7e206503bca75376ebe2b177e0
aguang0713 <a0638713@gmail.com>
2018-11-22 15:22:04 +0800

    12

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -22,6 +22,27 @@
 import sys
 import time
 import Adafruit_DHT
+import httplib, urllib
+import json
+deviceId = "DqAHkwXD “
+deviceKey = “T63AuKyiWIkLua0u“ 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print "Error: %s" % ex time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+
 
 
 # Parse command line parameters.
@@ -39,7 +60,7 @@ else:
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 while 1:
-	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -49,8 +70,14 @@ while 1:
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 	if humidity is not None and temperature is not None:
-    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+			   {"dataChnId":"Temperature","values":{"value":t0}}]} 
+		post_to_mcs(payload)
+		time.sleep(10) 
+
 	else:
-    		print('Failed to get reading. Try again!')
-    		sys.exit(1)
-	time.sleep(1)
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
+
